b9f24471a205f35af10d71ddd28cd1ac8607de5e
Phạm Bá Đạt <pham.ba.dat153@gmail.com>
2025-06-02 09:08:40 +0700

    Add files via upload

---
+++ group6/group6/src/main/java/com/uef/group6/LibraryWebApplication.java
@@ -0,0 +1,13 @@
+package com.uef.group6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryWebApplication.class, args);
+	}
+
+}
